cd3e423e9f7302724949e26ea39dbbf435d37d1f
Anıl Orhun DEMİROĞLU <aorhun.demiroglu@stm.com.tr>
2024-01-31 17:50:10 +0300

    last push for the day will be continued on composite pattern

--- DesignPatterns/include/Composite.h
+++ DesignPatterns/include/Composite.h
@@ -5,15 +5,58 @@
 #define Watt double
 #define Currency float
 
+#include <vector>
+// #include <iterator>
 
-class Equipment{
+template <typename T>
+class Iterator {
+public:
 
+    virtual bool HasNext() const = 0;
+    virtual T* Next() = 0;
+};
+
+class Equipment {
 public:
     virtual ~Equipment();
 
-    const char* Name() {return _name;}
+    const char* Name() const { return _name; }
+
+    virtual Watt Power() = 0;
+    virtual Currency NetPrice() = 0;
+    virtual Currency DiscountPrice() = 0;
+
+    virtual void Add(Equipment*);
+    virtual void Remove(Equipment*);
+    virtual Iterator<Equipment*>* CreateIterator() = 0;
+
+protected:
+    Equipment(const char*);
+
+private:
+    const char* _name;
+};
+
+
+class FloppyDisk : public Equipment {
+public:
+    FloppyDisk(const char*);
+    virtual ~FloppyDisk();
+    
+    virtual Watt Power();
+
+    virtual Currency NetPrice();
+    virtual Currency DiscountPrice();
+};
+
+
+class CompositeEquipment : public Equipment {
+public:
+
+    virtual ~CompositeEquipment();
 
     virtual Watt Power();
+
     virtual Currency NetPrice();
     virtual Currency DiscountPrice();
 
@@ -21,15 +64,12 @@ public:
     virtual void Remove(Equipment*);
     virtual Iterator<Equipment*>* CreateIterator();
 
-
 protected:
-    Equipment(const char*);
+    CompositeEquipment(const char*);
 
 private:
-    const char* _name;
-
+    std::vector<Equipment*> _equipment;
 
 };
-
 
 #endif

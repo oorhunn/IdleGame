8b1b8581b4569e7ddcb45194566a8be6465c2e82
Anıl Orhun DEMİROĞLU <aorhun.demiroglu@stm.com.tr>
2024-02-02 11:31:55 +0300

    strategy pattern finished

--- DesignPatterns/main.cpp
+++ DesignPatterns/main.cpp
@@ -1,20 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <array>
 #include "./include/StrategyPattern/Strategy.h"
 
-void clientCode()
-{
-    Context context(std::make_unique<ConcreteStrategyA>());
-    std::cout << "Client: Strategy is set to normal sorting.\n";
-    context.doSomeBusinessLogic();
-    std::cout << "\n";
-    std::cout << "Client: Strategy is set to reverse sorting.\n";
-    context.set_strategy(std::make_unique<ConcreteStrategyB>());
-    context.doSomeBusinessLogic();
-}
+
 
 int main()
 {
     clientCode();
+
+
     return 0;
 }
--- DesignPatterns/src/StrategyPattern/Strategy.cpp
+++ DesignPatterns/src/StrategyPattern/Strategy.cpp
@@ -36,3 +36,16 @@ std::string ConcreteStrategyB::doAlgorithm(std::string_view data) const {
 
     return result;    
 }
+
+
+
+void clientCode()
+{
+    Context context(std::make_unique<ConcreteStrategyA>());
+    std::cout << "Client: Strategy is set to normal sorting.\n";
+    context.doSomeBusinessLogic();
+    std::cout << "\n";
+    std::cout << "Client: Strategy is set to reverse sorting.\n";
+    context.set_strategy(std::make_unique<ConcreteStrategyB>());
+    context.doSomeBusinessLogic();
+}

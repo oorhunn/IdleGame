985deaf2ac0a257da8d51885aecb0f867d6e49f8
Anıl Orhun DEMİROĞLU <aorhun.demiroglu@stm.com.tr>
2024-02-12 16:59:01 +0300

    bridge pattern finished

--- DesignPatterns/main.cpp
+++ DesignPatterns/main.cpp
@@ -4,10 +4,21 @@
 #include <map>
 #include <algorithm>
 #include <unordered_map>
-#include "./include/AbstractPattern/Abstract.h"
-
+#include "./include/BridgePattern/Bridge.h"
 
 int main() {
+    Implementation* implementation = new ConcreteImplementationA;
+    Abstraction* abstraction = new Abstraction(implementation);
+    ClientCode(*abstraction);
+    std::cout << std::endl;
+    delete implementation;
+    delete abstraction;
 
+    implementation = new ConcreteImplementationB;
+    abstraction = new ExtendedAbstraction(implementation);
+    ClientCode(*abstraction);
+    delete implementation;
+    delete abstraction;
 
+    return 0;
 }

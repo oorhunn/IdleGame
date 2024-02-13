75edcdd43b1609ae368d87d42e43e54890c44784
Anıl Orhun Demiroğlu <49835142+oorhunn@users.noreply.github.com>
2024-02-13 15:30:01 +0300

    hel update

--- DesignPatterns/main.cpp
+++ DesignPatterns/main.cpp
@@ -13,11 +13,8 @@
 int main() {
     std::cout << "feuatuerue test \n";
     std::cout << "feuatuerue test 22 \n";
-    std::cout << "feuatuerue test 22 \n";
-    std::cout << "feuatuerue test 22 \n";
-    std::cout << "feuatuerue test 22 \n";
-    std::cout << "feuatuerue test 22 \n";
-    std::cout << "feuatuerue test 22 \n";
-    std::cout << "feuatuerue test 22 \n";
+    std::cout << "feuatuerue hel 22 \n";
+    std::cout << "feuatuerue hel 22 \n";
+
     return 0;
 }

0a5b7111a3458be196dea8d22d0e40e2ea7705d0
Anıl Orhun DEMİROĞLU <aorhun.demiroglu@stm.com.tr>
2024-02-13 12:11:52 +0300

    iterator pattern setup

--- DesignPatterns/main.cpp
+++ DesignPatterns/main.cpp
@@ -4,21 +4,13 @@
 #include <map>
 #include <algorithm>
 #include <unordered_map>
+#include <string>
+
+
 #include "./include/BridgePattern/Bridge.h"
 
-int main() {
-    Implementation* implementation = new ConcreteImplementationA;
-    Abstraction* abstraction = new Abstraction(implementation);
-    ClientCode(*abstraction);
-    std::cout << std::endl;
-    delete implementation;
-    delete abstraction;
 
-    implementation = new ConcreteImplementationB;
-    abstraction = new ExtendedAbstraction(implementation);
-    ClientCode(*abstraction);
-    delete implementation;
-    delete abstraction;
+int main() {
 
     return 0;
 }
---
+++ DesignPatterns/src/IteratorPattern/Iterator.cpp
@@ -0,0 +1,1 @@
+#include "../../include/IteratorPattern/Iterator.h"

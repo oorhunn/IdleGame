6f3bcc4f5d0141f7927310ee56ef093ba81e115b
Anıl Orhun DEMİROĞLU <aorhun.demiroglu@stm.com.tr>
2024-02-13 12:23:01 +0300

    iterator class refactoring done

--- DesignPatterns/src/IteratorPattern/Iterator.cpp
+++ DesignPatterns/src/IteratorPattern/Iterator.cpp
@@ -1,1 +1,34 @@
 #include "../../include/IteratorPattern/Iterator.h"
+
+
+
+template <typename T, typename U>
+Iterator<T, U>::Iterator(U *p_data, bool reverse = false) :
+    m_p_data_(p_data) 
+{
+    m_it_ = m_p_data_->m_data_.begin();
+
+}
+
+
+template <typename T, typename U>
+void Iterator<T, U>::First() {
+    m_it_ = m_p_data_->m_data_.begin();
+}
+
+template <typename T, typename U>
+void Iterator<T, U>::Next() {
+    m_it_++;
+}
+
+
+template <typename T, typename U>
+bool Iterator<T, U>::IsDone() {
+    return (m_it_ == m_p_data_->m_data_.end());
+}
+
+template <typename T, typename U>
+iter_type Iterator<T, U>::Current(){
+    return m_it_;
+}
+

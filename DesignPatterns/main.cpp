69e9c247f1bd55f59ebb03d41cd71e278c910587
Anıl Orhun Demiroğlu <49835142+oorhunn@users.noreply.github.com>
2024-02-13 15:58:34 +0300

    faruk test first push

--- DesignPatterns/main.cpp
+++ DesignPatterns/main.cpp
@@ -11,10 +11,7 @@
 
 
 int main() {
-    std::cout << "feuatuerue test \n";
-    std::cout << "feuatuerue test 22 \n";
-    std::cout << "feuatuerue hel 22 \n";
-    std::cout << "feuatuerue hel 22 \n";
+    std::cout << "faruk test \n";
 
     return 0;
 }
